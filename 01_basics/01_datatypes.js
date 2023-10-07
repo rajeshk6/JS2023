2a9cb2d13925bab99d57b672989614c4d3844f29
rajeshk6 <98652376+rajeshk6@users.noreply.github.com>
2023-10-07 22:06:08 +0530

    Update 01_datatypes.js

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -1,1 +1,7 @@
+//where const can not be changed
+const accountId = 73473728339;
+let accountEmail = "rajeshkumar@g.com";
+var accountPassword ="1384343";
+
+console.table([accountId , accountEmail,accountPassword]);
 
